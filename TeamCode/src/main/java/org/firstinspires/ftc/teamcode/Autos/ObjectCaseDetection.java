ef720155bebbfbf405d3b1abf148eec11d75a340
C0sM1n <bordeic4@gmail.com>
2024-01-11 13:46:11 +0200

    solved some bugs + auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/ObjectCaseDetection.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+public class ObjectCaseDetection {
+    public enum Team{
+        RED,
+        BLUE
+    }
+
+    public enum Case{
+        LEFT,
+        MIDDLE,
+        RIGHT
+    }
+    public Team team;
+    public ObjectCaseDetection(Team team){
+        // camera funny
+
+        this.team = team;
+    }
+
+    public void run(){
+
+    }
+
+    public Case getCase(){
+
+        return Case.LEFT;
+    }
+}
