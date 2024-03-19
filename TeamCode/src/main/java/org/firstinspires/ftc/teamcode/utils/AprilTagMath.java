fc7aa5b97080094aa33209d4d4f561c4fc44bdca
CodrinCrismariu <codrincrismariu@gmail.com>
2024-03-19 15:26:38 +0200

    cu comentarii

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AprilTagMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/AprilTagMath.java
@@ -21,17 +21,28 @@ public class AprilTagMath {
     public static double[] TAG_Y_OFFSET = {0, 0, 0, 0, -43, -43, -43};
 
     public static Pose2d poseFromTag(Pose2d robotPose, AprilTagDetection detection) {
+
+        // these are the x and y positions of the AprilTag relative
+        // to the robot's global shutter camera
         double tagX = detection.pose.z * AprilTagToINCHES;
         double tagY = detection.pose.x * AprilTagToINCHES;
 
+        // this is the current robot heading
         double robotHeading = robotPose.getHeading();
 
+        // because the global shutter camera is rotated at the same angle
+        // as the robot, we need to recompute the position vector of
+        // the AprilTag relative to our global starting position
         double x_displacement = cos(robotHeading) * tagX - sin(robotHeading) * tagY;
         double y_displacement = cos(robotHeading) * tagY + sin(robotHeading) * tagX;
 
+        // because the global shutter camera is not located at the
+        // center the robot, we need add an offset vector to translate the
+        // vector to the center of the robot
         double x_to_center = cos(robotHeading) * distanceToCenter;
         double y_to_center = sin(robotHeading) * distanceToCenter;
 
+        // return the final positions with the global offset of the AprilTags on the field
         return new Pose2d(-(x_displacement + x_to_center) + TAG_X_OFFSET[detection.id], -(y_displacement + y_to_center) + TAG_Y_OFFSET[detection.id], robotHeading);
     }
 }
